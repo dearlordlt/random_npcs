ba319547f2deb87e9b7bed692427773426b72143
ssliteris <ssliteris@exadel.com>
2019-01-05 22:39:49 +0200

    attack/defence tweaks

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ const npcs = new Vue({
                 const magicPower = Math.floor(Math.random()*4) - 1;
                 const newNpc = { 
                     name: generate_name('egyptian'), 
-                    att: Math.floor(Math.random()*4), 
-                    def: Math.floor(Math.random()*4),
+                    att: Math.floor(Math.random()*5) + 10, 
+                    def: Math.floor(Math.random()*5) + 10,
                     STR: 10 + Math.floor(Math.random()*4), 
                     move: 3 + Math.floor(Math.random()*4) + 1, 
                     weapon: this.weapons[Math.floor(Math.random()*this.weapons.length)], 
                     shield: Math.floor(Math.random()*3),
                     armor: Math.floor(Math.random()*3),
-                    hp: 10 + Math.floor(Math.random()*4), 
+                    hp: 6 + Math.floor(Math.random()*6), 
                     magic:  magicPower > 0 ? magicPower + ' ' + this.spells[Math.floor(Math.random()*this.spells.length)] : '-',
                     magicPower: magicPower
                 }
