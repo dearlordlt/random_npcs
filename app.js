e7b72315943921f77bc8bee823a81f103b3cd9dd
ssliteris <ssliteris@exadel.com>
2019-01-05 22:33:42 +0200

    tweaks

--- app.js
+++ app.js
@@ -34,15 +34,20 @@ const npcs = new Vue({
                     STR: 10 + Math.floor(Math.random()*4), 
                     move: 3 + Math.floor(Math.random()*4) + 1, 
                     weapon: this.weapons[Math.floor(Math.random()*this.weapons.length)], 
-                    shield: true,
+                    shield: Math.floor(Math.random()*3),
+                    armor: Math.floor(Math.random()*3),
                     hp: 10 + Math.floor(Math.random()*4), 
-                    magic:  magicPower > 0 ? magicPower + ' ' + this.spells[Math.floor(Math.random()*this.spells.length)] : '-'
+                    magic:  magicPower > 0 ? magicPower + ' ' + this.spells[Math.floor(Math.random()*this.spells.length)] : '-',
+                    magicPower: magicPower
                 }
                 this.npcs.push(newNpc);
             }
         },
         clear: function() {
             this.npcs = [];
+        },
+        kill: function(index) {
+            this.npcs[index].hp = 0;
         }
     }
 })
