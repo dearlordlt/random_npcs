ebd856bb098a4f130b57e835bc42d34aa9985dea
ssliteris <ssliteris@exadel.com>
2019-01-10 12:40:51 +0200

    updated descriptions

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const npcs = new Vue({
         npc_power: 0,
         weapons: [
             {name: 'dagger', description: 'R0/1, SW 0+BBT, TR 0+BBC, W0, +1 to attack from side hexes, +1 Armor Piercing TR'},
-            {name: 'sword', description: 'R1, SW 1+BBT, TR 1+BBC, W1, +1 to defence when using shield'},
-            {name: 'axe', description: 'R1, SW 2+BBC, W2, +2 to shield damage'},
-            {name: 'mace', description: 'R1, SW 3+TTTT, W3, Stun damage to head, +2 Fatigue'},
+            {name: 'sword', description: 'R1, SW 1+BBT, TR 1+BBC, W1, +1 to defence when using shield, +1 Attack with no offhand'},
+            {name: 'axe', description: 'R1, SW 2+BBC, W2, +2 to shield damage, +1 Armor Piercing'},
+            {name: 'mace', description: 'R1, SW 3+TTTT, W3, Stun damage to head, +2 Shock'},
             {name: 'spear', description: 'R2, TR 2+BBC, W2, +1W when used in one hand'},
             {name: 'bow', description: 'BBC, STR12, W2'},
             {name: 'th.spear', description: 'R:STR*2, BBC, W2'},
